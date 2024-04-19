05757dec54d1e1e79346eab7d745da1d8c5653cd
christopheilermann <user@purebase.de>
2024-04-20 00:59:01 +0200

    IMPLEMENT DRAG-END
    - completing dnd for children

--- lib/DndList3/DndList3.tsx
+++ lib/DndList3/DndList3.tsx
@@ -9,8 +9,8 @@ import {Category, ChemicalItem, DraggableItem} from './DndList3DataTypes';
 
 const categories: Category[] = [
     { id: '1', name: 'Catergory 1', children: ['1', '2', '3'] },
-    { id: '2', name: 'Category 2', children: [] },
-    { id: '3', name: 'Category 3', children: [] },
+    { id: '2', name: 'Category 2', children: ['4', '5', '6', '7', '8'] },
+    { id: '3', name: 'Category 3', children: ['9', '10', '11', '12'] },
 ];
 
 const chemicalItemList: ChemicalItem[] = [
@@ -18,8 +18,14 @@ const chemicalItemList: ChemicalItem[] = [
     { id: '2', position: 56, mass: 137.33, symbol: 'Ba', name: 'EVA' },
     { id: '3', position: 58, mass: 140.12, symbol: 'Ce', name: 'THOMAS' },
     { id: '4', position: 6, mass: 12.011, symbol: 'C', name: 'NICOLE' },
-/*        { id: '5', position: 7, mass: 14.007, symbol: 'N', name: 'WILMAR', parentId: '1' },
-        { id: '6', position: 8, mass: 14.007, symbol: 'N', name: 'MANUEL', parentId: '1' },*!/*/
+    { id: '5', position: 39, mass: 88.906, symbol: 'Y', name: 'MANUEL' },
+    { id: '6', position: 56, mass: 137.33, symbol: 'Ba', name: 'TINA' },
+    { id: '7', position: 58, mass: 140.12, symbol: 'Ce', name: 'LAURA' },
+    { id: '8', position: 6, mass: 12.011, symbol: 'C', name: 'WILLE' },
+    { id: '9', position: 39, mass: 88.906, symbol: 'Y', name: 'THEO' },
+    { id: '10', position: 56, mass: 137.33, symbol: 'Ba', name: 'EDITH' },
+    { id: '11', position: 58, mass: 140.12, symbol: 'Ce', name: 'DANI' },
+    { id: '12', position: 6, mass: 12.011, symbol: 'C', name: 'WILMAR' },
 ];
 
 const renderChemicalItem = (item: ChemicalItem) => (
@@ -73,51 +79,59 @@ export function DndList3Stage() {
 
         const start = depth1Typed.find(category => category.id === source.droppableId);
         const finish = depth1Typed.find(category => category.id === destination.droppableId);
+        if (!start || !finish) return;
 
-        if (start && finish && start === finish) {
+        if (start === finish) {
             const newChildIds = [...start.children];
             console.log('onDragEnd()#1.1', JSON.stringify(depth1Typed, null, 2));
-            const item = start.children[source.index];
+            const draggedItem = start.children[source.index];
 
+            // del and add in same child list
             newChildIds.splice(source.index, 1);
-            newChildIds.splice(destination.index, 0, item);
+            newChildIds.splice(destination.index, 0, draggedItem);
 
             const newCategory = {
                 ...start,
                 children: newChildIds,
             };
 
-            const newState = depth1Typed.map(currentCategory => (currentCategory.id === newCategory.id)
-                    ? newCategory : currentCategory);
+            const newState = depth1Typed
+                .map(currentCategory => {
+                    if (currentCategory.id === newCategory.id) return newCategory;
+                    return currentCategory;
+                });
 
             console.log('onDragEnd()#1.2', JSON.stringify(newState, null, 2));
             depth1Handlers.setState(newState);
-            //return;
-        }
-
-        // TODO Use drag end strategy of https://egghead.io/lessons/react-move-items-between-columns-with-react-beautiful-dnd-using-ondragend
-/*        const { source, destination, draggableId } = result;
-        console.log('onDragEnd()', source, destination, destination?.droppableId, draggableId);
-        if (!destination) {
             return;
         }
-        if (destination.droppableId === 'ROOT') {
-            depth1Handlers.reorder({ from: source.index, to: destination?.index || 0 });
-        } else if (destination.droppableId !== source.droppableId) {
-            const newDepth2 = depth2Typed.map((item) => {
-                if (item.id === draggableId) {
-                    return {
-                        ...item,
-                        parentId: destination.droppableId,
-                    };
-                }
-                    return item;
+
+        const startChildIds = [...start.children];
+        // Remember dragged item before deleting it:
+        const draggedItem = start.children[source.index];
+        // del in start child list:
+        startChildIds.splice(source.index, 1);
+        const startCategory = {
+            ...start,
+            children: startChildIds,
+        };
+
+        const finishChildIds = [...finish.children];
+        // del in start child list:
+        finishChildIds.splice(destination.index, 0, draggedItem);
+        const finishCategory = {
+            ...finish,
+            children: finishChildIds,
+        };
+
+        const newState = depth1Typed
+            .map(currentCategory => {
+                if (currentCategory.id === startCategory.id) return startCategory;
+                if (currentCategory.id === finishCategory.id) return finishCategory;
+                return currentCategory;
             });
-            console.log('onDragEnd()#2', JSON.stringify(newDepth2, null, 2));
-            depth2Handlers.setState(newDepth2);
-        } else {
-            depth2Handlers.reorder({ from: source.index, to: destination?.index || 0 });
-        }*/
+        console.log('onDragEnd()#2.2', JSON.stringify(newState, null, 2));
+        depth1Handlers.setState(newState);
     });
 
     return (
